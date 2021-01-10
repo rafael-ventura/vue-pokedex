1b547b2315a26a758b7c8b8413114e260159630d
Rafael Ventura <raf4ael@gmail.com>
2021-01-10 20:28:17 -0300

    aaaaa

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
     <div class="row">
       <div>
         <PokeCard
-          v-for="(pokemon, index) in searchResult()"
+          v-for="(pokemon, index) in pokemons"
           :key="index"
           v-bind:pokemon="pokemon"
         />

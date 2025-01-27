27b2b862628911d41263d242a1448dc600b9a912
Rafael Ventura <raf4ael@gmail.com>
2025-01-27 13:23:49 -0300

    .

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,16 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-pokedex/'
-    : '/'
-})
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-pokedex/' : '/',
+  outputDir: path.resolve(__dirname, 'dist'),
+  assetsDir: '', // Isso faz com que os arquivos sejam colocados diretamente no dist
+  filenameHashing: false, // Opcional, remove hashes dos nomes dos arquivos
+  configureWebpack: {
+    output: {
+      filename: '[name].js',
+      chunkFilename: '[name].js',
+    },
+  },
+});

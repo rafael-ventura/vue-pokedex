44c3434d8bf109c3e769488f0abd46a59cca298b
Rafael Ventura <raf4ael@gmail.com>
2025-01-27 14:01:43 -0300

    .

--- vue.config.js
+++ vue.config.js
@@ -1,46 +1,9 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
-  transpileDependencies: true,
-
-  // Caminho base para o projeto (ajustado para deploy estático)
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
-
-  // Diretório de saída
-  outputDir: 'dist',
-
-  // Evita subpastas para assets
-  assetsDir: '',
-
-  // Remove hashes opcionais para simplificar caminhos
-  filenameHashing: false,
-
-  // Configurações adicionais para Webpack
-  configureWebpack: {
-    output: {
-      filename: '[name].js',
-      chunkFilename: '[name].js',
-    },
-  },
-
-  // Configurações para lidar com imagens, fontes e SVGs
-  chainWebpack: (config) => {
-    config.module
-        .rule('images')
-        .set('generator', {
-          filename: '[name][ext]',
-        });
-
-    config.module
-        .rule('fonts')
-        .set('generator', {
-          filename: '[name][ext]',
-        });
-
-    config.module
-        .rule('svg')
-        .set('generator', {
-          filename: '[name][ext]',
-        });
-  },
+    transpileDependencies: true,
+    publicPath: './', // Caminhos relativos para produção
+    outputDir: 'dist', // Diretório de saída
+    assetsDir: '', // Coloca tudo no diretório `dist`, sem subpastas
+    filenameHashing: false, // Remove hashes opcionais
 });
